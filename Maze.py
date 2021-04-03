cf9dfe15fa519e41f806310221bc45b8fcc86a04
asomwaru <asomwaru01@gmail.com>
2021-04-03 03:40:09 -0400

    Added docstrings, comments, README

--- Maze.py
+++ Maze.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 
 
 class Node(object):
@@ -12,6 +12,9 @@ class Node(object):
         self.walls = [1, 1, 1, 1]
 
     def clear(self):
+        """
+        Resets the wall for the node
+        """
         self.walls = [1, 1, 1, 1]
 
     def __repr__(self):
@@ -34,21 +37,26 @@ class Node(object):
 
 
 class Maze(object):
-    def __init__(self, length: int = 5, width: int = 5,
-                 start_node=(0, 0)):
+    """A Maze object that holds a 2d array of nodes. Can generate a maze with 2 different methods.
+
+    Args:
+        length: the number of nodes long
+        width: the number of nodes wide
+    """
+
+    def __init__(self, length: int = 5, width: int = 5):
 
         self.length = length
         self.width = width
 
+        # create a 2d array of nodes
         self.grid = [[Node(x, y) for x in range(self.width)] for y in range(self.length)]
-
-        if type(start_node) in [list, tuple]:
-            self.start_node = self.grid[start_node[1]][start_node[0]]
-        elif type(start_node) == Node:
-            self.start_node = start_node
+        self.start_node = Node(0, 0)
 
     def change_wall(self, x: int, y: int, wall: int):
         if wall == 0:
+            
+            # Ensures if wall is edge of map or not
             if y == 0:
                 self.grid[y][x].walls[0] = int(not self.grid[y][x].walls[0])
             else:
@@ -56,6 +64,8 @@ class Maze(object):
                 self.grid[y - 1][x].walls[1] = int(not self.grid[y - 1][x].walls[1])
 
         elif wall == 1:
+
+            # Ensures if wall is edge of map or not
             if y == self.length - 1:
                 self.grid[y][x].walls[1] = int(not self.grid[y][x].walls[1])
             else:
@@ -63,6 +73,8 @@ class Maze(object):
                 self.grid[y + 1][x].walls[0] = int(not self.grid[y + 1][x].walls[0])
 
         elif wall == 2:
+            
+            # Ensures if wall is edge of map or not
             if x == self.width - 1:
                 self.grid[y][x].walls[2] = int(not self.grid[y][x].walls[2])
             else:
@@ -70,6 +82,8 @@ class Maze(object):
                 self.grid[y][x + 1].walls[3] = int(not self.grid[y][x + 1].walls[3])
 
         elif wall == 3:
+            
+            # Ensures if wall is edge of map or not
             if x == 0:
                 self.grid[y][x].walls[3] = int(not self.grid[y][x].walls[3])
             else:
@@ -77,6 +91,17 @@ class Maze(object):
                 self.grid[y][x - 1].walls[2] = int(not self.grid[y][x - 1].walls[2])
 
     def neighbors(self, x: int, y: int, diagonal: bool = False):
+        """A generator that yields neighboring values.
+        
+        Args:
+            x: x position of the array
+            y: y position of the array
+            diagonal: boolean if diagonal neighbors are allowed or not
+        
+        Returns:
+            neighboring_node: a node that is near the current node
+        """
+
         for yp in range(-1, 2):
             for xp in range(-1, 2):
                 if xp == 0 and yp == 0:
@@ -94,12 +119,35 @@ class Maze(object):
                 yield self.grid[yp + y][xp + x]
 
     def _wall_exists(self, first: Node, second: Node, wall: int) -> bool:
+        """
+        Private function that checks if there is a wall between two nodes
+
+        Args:
+            first: Node to compare
+            second: Node to compare
+            wall: wall index to check between the nodes
+        
+        Returns:
+            bool: if there is a wall exists or doesn't between the two nodes
+        """
+
+        # North and East walls always have their neighbor +1 from their direction
         if wall in [0, 2]:
             return first.walls[wall] == second.walls[wall + 1]
+
+        # South and West walls always have their neighbor -1 from their direction
         elif wall in [1, 3]:
             return first.walls[wall] == second.walls[wall - 1]
 
     def _convert_pos(self, position):
+        """
+        Get the directional difference between two nodes and 
+        Args:
+            position: a list with the difference in position of two nodes
+        
+        Returns:
+            index: wall index of where the wall should be removed
+        """
         directions = {
             "N": [0, -1],
             "S": [0, 1],
@@ -107,29 +155,29 @@ class Maze(object):
             "W": [-1, 0]
         }
 
-        coord = {
-            'N': 0,
-            'S': 1,
-            'E': 2,
-            'W': 3
-        }
-
-        wall = ''
-
         for x, y in directions.items():
             if position == y:
-                wall = x
-                break
-
-        wall = coord[wall]
-        return wall
+                return list(directions.keys()).index(x)
 
     def clear(self):
+        """
+        Resets the maze by resetting the walls for each node
+        """
+
         for y in range(self.length):
             for x in range(self.width):
                 self.grid[y][x].clear()
 
     def display_maze(self):
+        """
+        Print out maze to terminal
+        Args:
+            None
+        
+        Returns:
+            None
+        """
+
         row = [' ']
         for x in range(self.width):
             if self.grid[0][x].walls[0]:
@@ -145,6 +193,7 @@ class Maze(object):
             row = []
             for x in range(self.width):
                 current = self.grid[y][x]
+
                 if x == 0:
                     if current.walls[3] == 1:
                         row.append('|')
@@ -158,20 +207,33 @@ class Maze(object):
 
                 if current.walls[2] == 1 and x + 1 < self.width and self.grid[y][x + 1].walls[3] == 1:
                     row.append('|')
-                elif current.x == self.width - 1:
-                    if current.walls[2] == 1:
-                        row.append('|')
+
+                elif current.x == self.width - 1 and current.walls[2] == 1:
+                    row.append('|')
+
                 else:
                     row.append(' ')
 
             print("".join(row))
 
-    def convert_to_image(self, name:str='maze.png', save:bool=True):
+    def convert_to_image(self, name:str='maze.png', save:bool=True) -> Image:
+        """
+        Converts maze into an image. When saving, two images are generated 
+        where one is 1:1 to solve and another for actually viewing.
+
+        Args:
+            name: name for file output
+            save: boolean for saving to a file
+        
+        Returns:
+            image: Returns the Pillow image for possible uses
+        """
+        # setup a blank, white image
         l, w = (self.length * 2) + 1, (self.width * 2) + 1
-        offset = 1
         img = Image.new('RGB', (w, l), color=(255, 255, 255))
         arr = np.array(img)
 
+        # add a black border around the maze
         arr[0, :] = (0, 0, 0)
         arr[:, 0] = (0, 0, 0)
         arr[l - 1, :] = (0, 0, 0)
@@ -179,30 +241,62 @@ class Maze(object):
 
         for y in range(self.length):
             for x in range(self.width):
+
+                # create an array where all of the node positions of the image in the x and y axis are made
                 translate_y = [i + 1 for i in list(range(0, l - 1, 2))]
                 translate_x = [i + 1 for i in list(range(0, w - 1, 2))]
                 current = self.grid[y][x]
 
+                # if an east wall exists then fill in the nearby pixels with black
                 if current.walls[2]:
-                    arr[translate_y[y], translate_x[x] + offset] = (0, 0, 0)
-                    arr[translate_y[y] + offset, translate_x[x] + offset] = (0, 0, 0)
-                    arr[translate_y[y] - offset, translate_x[x] + offset] = (0, 0, 0)
+                    arr[translate_y[y], translate_x[x] + 1] = (0, 0, 0)
+                    arr[translate_y[y] + 1, translate_x[x] + 1] = (0, 0, 0)
+                    arr[translate_y[y] - 1, translate_x[x] + 1] = (0, 0, 0)
 
+                # if a south wall exists then fill in the nearby pixels with black
                 if current.walls[1]:
-                    arr[translate_y[y] + offset, translate_x[x] + offset] = (0, 0, 0)
-                    arr[translate_y[y] + offset, translate_x[x] - offset] = (0, 0, 0)
-                    arr[translate_y[y] + offset, translate_x[x]] = (0, 0, 0)
+                    arr[translate_y[y] + 1, translate_x[x] + 1] = (0, 0, 0)
+                    arr[translate_y[y] + 1, translate_x[x] - 1] = (0, 0, 0)
+                    arr[translate_y[y] + 1, translate_x[x]] = (0, 0, 0)
 
-        arr[0,1] = (0, 255, 0)
+        # places a green and red color for the start and end
+        arr[0, 1] = (0, 255, 0)
         arr[l - 1, w - 2] = (255, 0, 0)
 
+        # convert to image for potential uses
         new_img = Image.fromarray(arr.astype('uint8'), 'RGB')
+
         if save:
             new_img.save(name)
-        else:
-            return new_img
+            upscale = self.upscale_image(new_img)
+            upscale.save("upscaled.png")
 
-    def simple_ascii(self):
+        return new_img
+
+    def upscale_image(self, img:Image, scale:int=10) -> Image:
+        l, w = (self.length * 2) + 1, (self.width * 2) + 1
+
+        im = Image.new('RGB', (l*scale, w*scale), (0, 0, 0))
+        draw = ImageDraw.Draw(im)
+        arr = np.array(img)
+
+        for y in range(l):
+            for x in range(w):
+                draw.rectangle([(x * scale, y * scale), ((x + 1) * scale, (y + 1) * scale)], fill=tuple(arr[y,x]))
+
+        return im
+
+    def simple_ascii(self, name:str="maze.txt", save:bool=True):
+        """Generates maze as a string array.
+
+        Args:
+            name: name of the text file for the maze
+            save: boolean to save the maze as a text file
+
+        Returns:
+            arr: string array
+        """
+
         l, w = (self.length * 2) + 1, (self.width * 2) + 1
 
         arr = [['.' for _ in range(w)] for _ in range(l)]
@@ -233,9 +327,25 @@ class Maze(object):
 
         arr[0][1] = 'S'
         arr[l - 1][w - 2] = 'E'
+
+        if save:
+            with open(f"{name}", "w") as fh:
+                for x in arr:
+                    fh.write(f"{' '.join(x)}\n")
+
         return arr
 
     def read_picture(self, name:str='maze.png'):
+        """
+        Reads in an image of a maze. Maze must be 1:1 in terms of pixel to node
+
+        Args:  
+            Name: name of the input image
+        
+        Returns: 
+            None
+        """
+
         img = Image.open(name)
         arr = np.array(img)
 
@@ -275,17 +385,24 @@ class Maze(object):
         return new_pos
 
     def iterative_backtrack(self):
+        """Uses backtracking to explore all nodes randomly to generate maze."""
+
+        # start stack with the starting node and an empty array of visited positions
         stack = [self.start_node]
         visited = []
 
         while len(stack) > 0:
+            # remove node from stack and add it to visited array
             current = stack.pop()
             visited.append((current.x, current.y))
 
+            # get available neighbors and randomly shuffle them
             available_nodes = [x for x in self.neighbors(current.x, current.y) if not (x.x, x.y) in visited]
             random.shuffle(available_nodes)
 
+            # checks if there are at least 1 available node to visit
             if len(available_nodes) > 0:
+                # add first node to the stack and get it's direction from current node
                 stack.append(current)
                 next_cell = available_nodes.pop()
                 position = next_cell - current
@@ -312,17 +429,12 @@ class Maze(object):
             visited.append((current.x, current.y))
             current = next_node
 
-    # Unfinished
-    def hunt_kill(self):
-        pass
-
 
 def main():
-    m = Maze(length=15, width=15)
+    m = Maze(length=25, width=25)
+    m.Aldous_Broder()
 
-    m.read_picture('maze.png')
-
-    m.convert_to_image()
+    m.convert_to_image(save=True)
 
 
 if __name__ == '__main__':
--- Solver.py
+++ Solver.py
@@ -1,47 +1,48 @@
 import Maze
 import numpy as np
 from PIL import Image, ImageDraw
-from pprint import pprint
+
+
+# created a separate node object for solving so that it would be easier to retrace steps after solving
+class Node(object):
+    def __init__(self, pos:list, edges:list):
+        self.pos = pos
+        self.parent = None
+        self.edges = edges
+
+    def __eq__(self, other):
+        if other == None:
+            return False
+        elif type(other) == list:
+            return self.pos == other
+        else:
+            return other.pos == self.pos
+
+    def __neq__(self, other):
+        if other == None:
+            return False
+        elif type(other) == list:
+            return self.pos != other
+        else:
+            return other.pos != self.pos
+
+    def __getitem__(self, index: int):
+        return self.pos[index]
+
+    def __add__(self, other):
+        return [self.pos[0] + other.pos[0], self.pos[1] + other.pos[1]]
+
+    def __sub__(self, other):
+        return [self.pos[0] - other.pos[0], self.pos[1] - other.pos[1]]
+
+    def __repr__(self):
+        return str(self.pos)
+
+    def __str__(self):
+        return str(self.pos)
 
 
 class Maze_Solver(object):
-    class Node(object):
-        def __init__(self, pos:list, edges:list):
-            self.pos = pos
-            self.parent = None
-            self.edges = edges
-
-        def __eq__(self, other):
-            if other == None:
-                return False
-            elif type(other) == list:
-                return self.pos == other
-            else:
-                return other.pos == self.pos
-
-        def __neq__(self, other):
-            if other == None:
-                return False
-            elif type(other) == list:
-                return self.pos != other
-            else:
-                return other.pos != self.pos
-
-        def __getitem__(self, index: int):  # Apparently it can do it for any amount of arguments
-            return self.pos[index]
-
-        def __add__(self, other):
-            return [self.pos[0] + other.pos[0], self.pos[1] + other.pos[1]]
-
-        def __sub__(self, other):
-            return [self.pos[0] - other.pos[0], self.pos[1] - other.pos[1]]
-
-        def __repr__(self):
-            return str(self.pos)
-
-        def __str__(self):
-            return str(self.pos)
-
     def __init__(self, maze:Maze.Maze):
         self.maze = maze
 
@@ -51,24 +52,29 @@ class Maze_Solver(object):
 
         self.width = self.maze.width
         self.length = self.maze.length
+
         self.start_pos = None
         self.exit_pos = None
+
+        # create a simple numpy array to map out the corridors and walls
         self.board = self._get_2d()
 
+        # create a node graph where there are only important nodes 
         self._create_graph()
 
     def _get_2d(self):
-        ascii = self.maze.simple_ascii()
-        zero = np.zeros((len(ascii[0]), len(ascii)))
+        text_maze = self.maze.simple_ascii(save=False)
+        zero = np.zeros((len(text_maze[0]), len(text_maze)))
 
-        for y in range(len(ascii)):
-            for x in range(len(ascii[y])):
-                if ascii[y][x] == '.':
+        for y in range(len(text_maze)):
+            for x in range(len(text_maze[y])):
+
+                if text_maze[y][x] == '.':
                     zero[y, x] = 1
-                elif ascii[y][x] == 'S':
+                elif text_maze[y][x] == 'S':
                     self.start_pos = [x, y]
                     zero[y, x] = 1
-                elif ascii[y][x] == 'E':
+                elif text_maze[y][x] == 'E':
                     self.exit_pos = [x, y]
                     zero[y, x] = 1
 
@@ -113,7 +119,7 @@ class Maze_Solver(object):
             previous = self._look_around(current)
 
             # filter out local dots by if they're not visited and white
-            local_dots = list(map(list, self.neighbours(current[0], current[1])))
+            local_dots = list(map(list, self.neighbors(current[0], current[1])))
             local_dots = list(filter(lambda i: self.board[i[1], i[0]] == 1 and i not in visited, local_dots))
 
             if len(local_dots) > 1: # choose first local dot and put rest in queue
@@ -135,7 +141,7 @@ class Maze_Solver(object):
 
             # get local dots for new position and do same filtering
             next_pos = self._add_directions(current, dir)
-            local_dots = list(map(list, self.neighbours(next_pos[0], next_pos[1])))
+            local_dots = list(map(list, self.neighbors(next_pos[0], next_pos[1])))
             local_dots = list(filter(lambda x: self.board[x[1], x[0]] == 1 and x not in visited, local_dots))
 
             # continue walk until new discovery
@@ -147,7 +153,7 @@ class Maze_Solver(object):
 
                 visited.append(next_pos)
                 next_pos = self._add_directions(next_pos, dir)
-                local_dots = list(map(list, self.neighbours(next_pos[0], next_pos[1])))
+                local_dots = list(map(list, self.neighbors(next_pos[0], next_pos[1])))
                 local_dots = list(filter(lambda x: self.board[x[1], x[0]] == 1 and x not in visited, local_dots))
 
             # add new discovery to vertices
@@ -171,21 +177,32 @@ class Maze_Solver(object):
         temp = []
         for x in self.vertices:
             edges = [y for y in self.edges if x == y[0]]
-            temp.append(self.Node(x, edges))
+            temp.append(Node(x, edges))
 
         self.vertices = temp
         for x in self.vertices:
-            if x == self.Node(self.start_pos, []):
+            if x == Node(self.start_pos, []):
                 self.start_pos = x
                 break
 
         for x in self.vertices:
-            if x == self.Node(self.exit_pos, []):
+            if x == Node(self.exit_pos, []):
                 self.exit_pos = x
                 break
 
 
-    def neighbours(self, x:int, y:int):
+    def neighbors(self, x:int, y:int):
+        """A generator that yields neighboring values.
+        
+        Args:
+            x: x position of the array
+            y: y position of the array
+        
+        Returns:
+            x_change: the neighboring node in the x position
+            y_change: the neighboring node in the y position
+        """
+
         l, w = self.board.shape
 
         for yp in range(-1, 2):
@@ -202,6 +219,8 @@ class Maze_Solver(object):
                 yield xp + x, yp + y
 
     def clear(self):
+        """Undoes the solving of the maze."""
+
         for x in self.vertices:
             x.parent = None
 
@@ -229,7 +248,7 @@ class Maze_Solver(object):
 
         return Image.fromarray(arr.astype('uint8'), 'RGB')
 
-    def _upscale_image(self, img:Image, scale:int=10):
+    def _upscale_image(self, img:Image, scale:int=10) -> Image:
         l, w = self.board.shape
 
         im = Image.new('RGB', (l*scale, w*scale), (0, 0, 0))
@@ -262,6 +281,7 @@ class Maze_Solver(object):
             if len(nearby_nodes) == 1:
                 nearby_nodes[0].parent = current
                 queue.append(nearby_nodes[0])
+
             elif len(nearby_nodes) > 1:
                 for x in nearby_nodes:
                     x.parent = current
@@ -296,6 +316,7 @@ class Maze_Solver(object):
             if len(nearby_nodes) == 1:
                 nearby_nodes[0].parent = current
                 stack.append(nearby_nodes[0])
+
             elif len(nearby_nodes) > 1:
                 for x in nearby_nodes:
                     x.parent = current
@@ -325,7 +346,7 @@ class Maze_Solver(object):
         images[0].save('walk.gif',
                        save_all=True, append_images=images[1:], optimize=False, duration=duration, loop=0)
 
-    def show_path(self, upscale:bool=False):
+    def show_path(self, upscale:bool=True):
         l, w = self.board.shape
 
         img = Image.new('RGB', (w, l), color=(0, 0, 0))
@@ -370,18 +391,19 @@ class Maze_Solver(object):
 
 def main():
     m = Maze.Maze(length=35, width=35)
-    # m.read_picture()
+    m.read_picture()
 
-    print("Read the image in")
-    m.iterative_backtrack()
-    m.convert_to_image()
+    # print("Read the image in")
+    # m.iterative_backtrack()
+    # m.convert_to_image()
 
     sol = Maze_Solver(m)
     print("Solving maze")
-    sol.DFS()
+    sol.BFS()
 
     print("Saving the maze")
     sol.show_path()
+    sol.walk_animation()
 
 
 if __name__ == '__main__':
